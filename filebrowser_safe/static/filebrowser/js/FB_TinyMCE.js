ab9942bfbfec374906439cb22f44311cc014141e
manojlds <manojlds@gmail.com>
2013-05-29 09:21:13 +0530

    Upgrading to support TinyMCE 4

--- filebrowser_safe/static/filebrowser/js/FB_TinyMCE.js
+++ filebrowser_safe/static/filebrowser/js/FB_TinyMCE.js
@@ -1,28 +1,8 @@
 var FileBrowserDialogue = {
-    init : function () {
-        // remove tinymce stylesheet.
-        var allLinks = document.getElementsByTagName("link");
-        allLinks[allLinks.length-1].parentNode.removeChild(allLinks[allLinks.length-1]);
-    },
     fileSubmit : function (FileURL) {
-        var URL = FileURL;
-        var win = tinyMCEPopup.getWindowArg("window");
-        
-        // insert information now
-        win.document.getElementById(tinyMCEPopup.getWindowArg("input")).value = URL;
-        
-        // change width/height & show preview
-        if (win.ImageDialog){
-            if (win.ImageDialog.getImageData)
-                win.ImageDialog.getImageData();
-            if (win.ImageDialog.showPreviewImage)
-                win.ImageDialog.showPreviewImage(URL);
-        }
-        
-        // close popup window
-        tinyMCEPopup.close();
-    }
+        this.parentWin.document.getElementById(this.parentWin.tinymce_activeInput).value = FileURL;
+        this.parentWin.tinymce_activeFrame.close();
+    },
+
+    parentWin: (!window.frameElement && window.dialogArguments) || opener || parent || top
 }
-
-tinyMCEPopup.onInit.add(FileBrowserDialogue.init, FileBrowserDialogue);
-
