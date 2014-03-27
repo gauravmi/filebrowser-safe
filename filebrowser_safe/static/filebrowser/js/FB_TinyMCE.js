ec397008be109501de1fcc83a517580462f216e0
Devarajan Narayanan <dvrajan12@gmail.com>
2014-03-27 15:48:43 +0530

    Upgrading to support TinyMCE 4

--- filebrowser_safe/static/filebrowser/js/FB_TinyMCE.js
+++ filebrowser_safe/static/filebrowser/js/FB_TinyMCE.js
@@ -3,6 +3,5 @@ var FileBrowserDialogue = {
         this.parentWin.document.getElementById(this.parentWin.tinymce_activeInput).value = FileURL;
         this.parentWin.tinymce_activeFrame.close();
     },
-
     parentWin: (!window.frameElement && window.dialogArguments) || opener || parent || top
 }
